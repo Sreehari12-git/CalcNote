3e21d3c05606e9d2a7b1966f73ee00019050b57d
Sreehari K P <sreehari.kp@gnapi.tech>
2026-04-30 18:07:45 +0530

    password generator modfied

--- dashboard/src/Components/Password.jsx
+++ dashboard/src/Components/Password.jsx
@@ -1,9 +1,8 @@
-
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useState } from 'react'
 
 function Password() {
     const [length, setLength] = useState(3);
-    const [ includeLetters, setLetters] = useState(false)
+    const [includeLetters, setLetters] = useState(false);
     const [includeNumbers, setIncludeNumbers] = useState(false);
     const [includeSpecials, setIncludeSpecials] = useState(false);
     const [result, setResult] = useState("");
@@ -14,47 +13,71 @@ function Password() {
         let specials = "!@#$%^&*()_+[]{}|;:,.<>?";
 
         let allChars = "";
-        if(includeLetters) allChars += letters;
-        if(includeNumbers) allChars += numbers;
-        if(includeSpecials) allChars += specials;
+        if (includeLetters) allChars += letters;
+        if (includeNumbers) allChars += numbers;
+        if (includeSpecials) allChars += specials;
 
-        if(!allChars) {
+        if (!allChars) {
             alert("Please select at least one character type");
             return;
         }
 
-        let generatePassword = "";
-        for(let i = 0; i < length; i++) {
+        let password = ""; 
+        for (let i = 0; i < length; i++) {
             const randomIndex = Math.floor(Math.random() * allChars.length);
-            generatePassword += allChars[randomIndex];
+            password += allChars[randomIndex];
         }
-        setResult(generatePassword);
-    }
-  return (
-    <>
-        <label>
-            <input type='checkbox' checked = {includeLetters} onChange={() => setLetters(!includeLetters)}/>
-            Letters
-        </label>
-        <label>
-            <input type='checkbox' checked = {includeNumbers} onChange={() => setIncludeNumbers(!includeNumbers)}/>
-            Numbers
-        </label>
-        <label>
-            <input type='checkbox' checked = {includeSpecials} onChange={() => setIncludeSpecials(!includeSpecials)}/>
-            Special symbols
-        </label>
-      <label>
-        <input type='range'
-            min="3"
-            max="10"/>
-      </label>
-      <p>Password: {result}</p>
-      <button onClick={generatePassword}>
-            Generate Password
-        </button>
-    </>
-  )
+
+        setResult(password);
+    };
+
+    return (
+        <>
+            <label>
+                <input
+                    type='checkbox'
+                    checked={includeLetters}
+                    onChange={() => setLetters(!includeLetters)}
+                />
+                Letters
+            </label>
+
+            <label>
+                <input
+                    type='checkbox'
+                    checked={includeNumbers}
+                    onChange={() => setIncludeNumbers(!includeNumbers)}
+                />
+                Numbers
+            </label>
+
+            <label>
+                <input
+                    type='checkbox'
+                    checked={includeSpecials}
+                    onChange={() => setIncludeSpecials(!includeSpecials)}
+                />
+                Special symbols
+            </label>
+
+            <label>
+                <input
+                    type='range'
+                    min="3"
+                    max="10"
+                    value={length}
+                    onChange={(e) => setLength(Number(e.target.value))} 
+                />
+                Length: {length}
+            </label>
+
+            <p>Password: {result}</p>
+
+            <button onClick={generatePassword}>
+                Generate Password
+            </button>
+        </>
+    );
 }
 
-export default Password
+export default Password;
